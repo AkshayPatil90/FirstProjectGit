65821f6cb77ac7aa50925d5329ee6d4f086e2f79
AkshayPatil90 <56379771+AkshayPatil90@users.noreply.github.com>
2023-02-28 08:33:38 +0100

    Java Interview question

---
+++ Projects/Java/Interview Questions/Sorting Binary Array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+	public static void main(String args[]) {
+		// Your code goes here
+		int[] a = {1,0,0,1,1,0,1,0};
+		Arrays.sort(a);
+		for (int num : a){
+			System.out.println(num);
+		}
+      
+	}
+}
